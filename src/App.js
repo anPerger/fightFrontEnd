f0167296e88b5e9933fbb27ea451291a779af1dd
audric <audricperger@gmail.com>
2022-03-17 15:58:04 -0700

    where's home?

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
    
           <Routes>
             
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/fighters" element={<Fighters />} />
           
           </Routes>
